1c7ba1ba8cbdd970b6ace5aaa3d83a804c8e80b2
Wesley de Carvalho Augusto Correia <wesley.correia@faculdadegran.edu.br>
2025-02-17 13:27:38 -0300

    modified:   src/app/app.component.html 	deleted:    src/app/app.component.spec.ts 	modified:   src/app/app.component.ts 	modified:   src/app/user/user.component.html 	modified:   src/app/user/user.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Signal, Input, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, input, computed} from '@angular/core';
 import {DUMMY_USERS} from '../dummy-users'
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -12,11 +12,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 export class UserComponent {
 
-  @Input() avatar!: string;
-  @Input() name!: string;
-  get imagePath() {
+   @Input({required: true}) avatar!: string;
+   @Input({ required: true }) name!: string;
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  imagePath = computed(() => {
     return 'assets/users/' + this.avatar;
-  }
+  });
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar;
+  // }
 
   onSelectUser() {
 
