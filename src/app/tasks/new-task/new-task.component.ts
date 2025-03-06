58acd807d157b2160934a69ba6e26e5448110394
Wesley de Carvalho Augusto Correia <wesley.correia@faculdadegran.edu.br>
2025-03-06 01:54:18 +0000

    new file:   src/app/shared/card/card.component.css 	new file:   src/app/shared/card/card.component.html 	new file:   src/app/shared/card/card.component.ts 	modified:   src/app/tasks/new-task/new-task.component.html 	modified:   src/app/tasks/new-task/new-task.component.ts 	modified:   src/app/tasks/task/task.component.css 	modified:   src/app/tasks/task/task.component.html 	modified:   src/app/tasks/task/task.component.ts 	modified:   src/app/tasks/task/task.model.ts 	modified:   src/app/tasks/tasks.component.html 	modified:   src/app/tasks/tasks.component.ts 	new file:   src/app/tasks/tasks.service.ts 	modified:   src/app/user/user.component.css 	modified:   src/app/user/user.component.html 	modified:   src/app/user/user.component.ts

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Output, } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { type NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -8,12 +10,26 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
+  @Input({required: true}) userId!: string;
+  @Output() close = new EventEmitter<void>();
 
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
+  private tasksService = inject(TasksService);
 
   onCancel() {
-    this.cancel.emit();}
-  }
+    this.close.emit();}
+
+  onSubmit() {
+    this.tasksService.addTask({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    },
+    this.userId
+  );
+  this.close.emit();
+  };
+}
+
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,18 +1,21 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { type Task } from './task.model';
-import  { NewTaskComponent } from '../new-task/new-task.component';
+import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
 @Input ({ required:true }) task!: Task;
-@Output() complete = new EventEmitter<string>();
-@Output() isAddingTask = new EventEmitter<string>();
+
+private tasksService = inject(TasksService);
 
 onCompleteTask() {
-  this.complete.emit(this.task.id);}
+  this.tasksService.removeTask(this.task.id);
+}
 }
